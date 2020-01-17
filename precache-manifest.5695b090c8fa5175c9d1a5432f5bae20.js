285e2fe1f327a201585b990c22b8e3efe31046fe
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-01-17 23:13:07 +0530

    Updates

---
+++ precache-manifest.5695b090c8fa5175c9d1a5432f5bae20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2e33f6ebdc9a5cdc4abfc6777371c8",
+    "url": "/wedding-planner/index.html"
+  },
+  {
+    "revision": "befcdf3e20026f748c82",
+    "url": "/wedding-planner/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5a91ade8dc090cea0a65",
+    "url": "/wedding-planner/static/js/2.04caf1d4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wedding-planner/static/js/2.04caf1d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "befcdf3e20026f748c82",
+    "url": "/wedding-planner/static/js/main.127cad44.chunk.js"
+  },
+  {
+    "revision": "996cd61e83090c606897",
+    "url": "/wedding-planner/static/js/runtime-main.0a33869f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wedding-planner/static/media/logo.5d5d9eef.svg"
+  }
+]);
