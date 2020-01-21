e487b0827c27194443167b7566eee95778c085c2
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-01-21 10:47:41 +0530

    ui changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,8 @@ class Home extends Component{
     render() {
         return (
             <div>
-                <div>
-                <img src="https://media.weddingz.in/images/66e4ddd34b6cbfd42e60fbae48e85248/shweta-and-arjun-lakshmi-niwas-palace-bikaner-wedding.jpg" class="img-fluid" alt="Responsive image" />
-                
+                <div class="jumbotron justify-content-center" style={{backgroundImage:"url('https://media.weddingz.in/images/66e4ddd34b6cbfd42e60fbae48e85248/shweta-and-arjun-lakshmi-niwas-palace-bikaner-wedding.jpg')", backgroundSize:'cover', backgroundPosition:'center', backgroundRepeat:'no-repeat' }}>
+                <div class="display-1 d-flex text-light justify-content-center">WEDDING PLANNER</div>
                 </div>
                 <div class="display-1 d-flex justify-content-center" style={{width:'100%'}}>CITIES</div>
                 <SelectBanquetCity />
